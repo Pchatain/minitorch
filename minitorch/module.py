3d133d1c7544b05e37f58deb6adb8dc12f961cbd
Peter Chatain <pchatain@stanford.edu>
2022-03-20 14:02:13 -0700

    Finished task0_4 functionality

--- minitorch/module.py
+++ minitorch/module.py
@@ -21,13 +21,15 @@ class Module:
 
     def train(self):
         "Set the mode of this module and all descendent modules to `train`."
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        self.training = True
+        for sub_module in self.modules():
+            sub_module.train()
 
     def eval(self):
         "Set the mode of this module and all descendent modules to `eval`."
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        self.training = False
+        for sub_module in self.modules():
+            sub_module.eval()
 
     def named_parameters(self):
         """
@@ -37,13 +39,24 @@ class Module:
         Returns:
             list of pairs: Contains the name and :class:`Parameter` of each ancestor parameter.
         """
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        all_prarms = []
+        for name, param in self.__dict__["_parameters"].items():
+            all_prarms.append((name, param))
+
+        for module_name, module in self.__dict__["_modules"].items():
+            child_params = module.named_parameters()
+            if child_params is None:
+                continue  # not convinced this is necessary, but it can't hurt
+            for name, param in child_params:
+                new_name = module_name + "." + name
+                all_prarms.append((new_name, param))
+        return all_prarms
 
     def parameters(self):
         "Enumerate over all the parameters of this module and its descendents."
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        # for name, param in self.named_parameters():
+        #     yield param
+        return self.named_parameters()
 
     def add_parameter(self, k, v):
         """
